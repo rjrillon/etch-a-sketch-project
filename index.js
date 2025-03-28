2326295a976587cad2370b082500a5c05bbcad43
Robin <rjrillon97@gmail.com>
2025-03-28 01:06:15 -0700

    Boilerplate Created
    Includes function to create grid with basic styling.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("JS loaded..");
+
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 0; i < 16 * 16; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  gridContainer.appendChild(square);
+}
