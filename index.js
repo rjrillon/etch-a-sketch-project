d439ec051812c886b2caab8e16eb0a84e11c2b48
Robin <rjrillon97@gmail.com>
2025-03-28 14:01:02 -0700

    Basic 'sketch' feature added
    (1st commit on new branch)
    - Used event delegation to add single event listener
    for 'mouseover' that changes color of square(s).
    - Must refresh to 'erase' sketches

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ console.log("JS loaded..");
 
 const gridContainer = document.querySelector(".grid-container");
 
+// Dynamically creating grid
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   gridContainer.appendChild(square);
 }
+
+// Event delegation:
+// Add a SINGLE event listener to the grid container
+gridContainer.addEventListener("mouseover", event => {
+  //Check if the hovered element is a square, then change color.
+  if (event.target.classList.contains("square")) {
+    event.target.style.backgroundColor = "red";
+  }
+});
