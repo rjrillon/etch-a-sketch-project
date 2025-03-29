d71cd8e9b95d3244e228d59a7289ff09c0e73ac7
Robin <rjrillon97@gmail.com>
2025-03-28 21:44:37 -0700

    Updated 'sketch' effect on grid
    Instead of hover to sketch, it is now click + hold.

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ for (let i = 0; i < 16 * 16; i++) {
   gridContainer.appendChild(square);
 }
 
+let isMouseDown = false;
+gridContainer.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+gridContainer.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
 // Event delegation:
 // Add a SINGLE event listener to the grid container
 gridContainer.addEventListener("mouseover", event => {
   //Check if the hovered element is a square, then change color.
-  if (event.target.classList.contains("square")) {
+  if (isMouseDown && event.target.classList.contains("square")) {
     event.target.style.backgroundColor = "red";
   }
 });
